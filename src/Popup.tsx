808a5bda3a9e1c7f638ad1b04746ae33b928aac3
Apicha Preechatanakun <apicha.pre@m.swu.ac.th>
2025-04-29 22:57:37 +0700

    fix:about us message

--- src/Popup.tsx
+++ src/Popup.tsx
@@ -17,17 +17,23 @@ function Popup({togglePopup, popupVisible} : Props) {
             </div>
             {popupVisible && (
                 <div className="fixed inset-0 flex items-center justify-center bg-opacity-50 z-50">
-                    <div className="bg-white rounded-lg p-6 max-w-md shadow-lg">
+                    <div className="bg-white rounded-lg p-6 max-w-xl shadow-lg">
                         <h2 className="text-xl font-bold mb-4">About This Project</h2>
-                        <p className="text-gray-700">
-                            อธิบาย maybe วิธีใช้งานคร่าวๆรายละเอียด
+                        <p className="text-gray-700 text-justify leading-relaxed space-y-4">
+                        เป็นเว็บแอพพลิเคชันสำหรับวิเคราะห์ข้อความที่อาจจะเกี่ยวกับความซึมเศร้า<br/>
+                        จัดทำโดยนิสิตนักศึกษามหาวิทยาลัยศรีนครินทรวิโรฒ<br/>
+                        ผลงานชิ้นนี้เป็นส่วนหนึ่งของวิจัยปริญญาตรี<br/>
+                        หากมีข้อผิดพลาดประการใด คณะผู้จัดทำต้องขออภัยมา ณ ที่นี้
                         </p>
-                        <button
-                            onClick={togglePopup}
-                            className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-                        >
-                            Close
-                        </button>
+                        <div className="flex justify-between">
+                            <button
+                                onClick={togglePopup}
+                                className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                            >
+                                Close
+                            </button>
+                            <p className="text-gray-700 mt-4">คณะผู้จัดทำ</p>
+                        </div>
                     </div>
                 </div>
             )}
